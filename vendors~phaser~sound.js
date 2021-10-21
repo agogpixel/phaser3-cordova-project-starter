31d8fc9aee0b8f5e61ca87eba80a17c99124035e
kidthales <kidthales@users.noreply.github.com>
2021-10-21 03:41:16 +0000

    Deploying to gh-pages from @ agogpixel/phaser3-cordova-project-starter@4e4b6715f3f475e5b4aec370e0fe382c5625b772 🚀

---
+++ vendors~phaser~sound.js
@@ -0,0 +1,2 @@
+/*! For license information please see vendors~phaser~sound.js.LICENSE.txt */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{1497:function(o,n,d){o.exports={SoundManagerCreator:d(443),Events:d(76),BaseSound:d(154),BaseSoundManager:d(153),WebAudioSound:d(451),WebAudioSoundManager:d(449),HTML5AudioSound:d(446),HTML5AudioSoundManager:d(444),NoAudioSound:d(448),NoAudioSoundManager:d(447)}}}]);
